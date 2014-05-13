289995c7d7305e0978ae0cd8379dca3f9f80118c
unknown <ehamburger2015@room200-09.btfiloh.org>
2014-05-13 14:31:03 -0400

    Code inserted

--- BrokenCode/src/Country.java
+++ BrokenCode/src/Country.java
@@ -8,10 +8,52 @@ public class Country
     // 2. Name
     // 3. Is in UN
     // 4. Square Area
+    private int population;
+    private String name;
+    private boolean isInUN;
+    private int squareArea;
     
     //Methods
     // 1. Change NAme
     // 2. Add to UN
     // 3. Accessor and mutator methods for each instance varibale
+    public void changeName(String name)
+    {
+    	this.name = name;
+    }
+
+    public void addToUN()
+    {
+    	isInUN = true;
+    }
+
+    public int getPopulation()
+    {
+    	return population;
+    }
+
+    public String getName()
+    {
+    	return name;
+    }
     
+    public boolean isInUN()
+    {
+    	return isInUN;
+    }
+
+    public int getArea()
+    {
+    	return squareArea;
+    }
+
+    public void changePopulation(int pop)
+    {
+    	population = pop;
+    }
+
+    public void changeArea(int area)
+    {
+    	squareArea = area;
+    }
 }
